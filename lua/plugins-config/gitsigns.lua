a59672090547673083e90b322747ca66f7975736
pfma <45191163+PengfeiM@users.noreply.github.com>
2024-12-04 12:14:59 +0800

    feat(git): add key to nav git hunk

--- lua/plugins-config/gitsigns.lua
+++ lua/plugins-config/gitsigns.lua
@@ -1,1 +1,14 @@
-require('gitsigns').setup()
+local gitsigns = require("gitsigns")
+
+
+gitsigns.setup({
+
+    -- add key-binding on start up
+    -- may encapsule a better start up function in the future
+    -- on_attach = require("keybinding").gitSignsKeys(bufnr, )
+    on_attach = function(bufnr)
+        local keybinding = require("keybinding")
+
+        keybinding.gitSignsKeys(bufnr, gitsigns)
+    end
+})
