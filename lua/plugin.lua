34f297595ceeb5e173a560e980b2078984133d6f
pfma <45191163+PengfeiM@users.noreply.github.com>
2025-05-15 11:54:18 +0800

    feat: auto save

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -55,6 +55,22 @@ require("lazy").setup({
     },
     -- =======================================================
 
+    -- function
+    -- ======================================================
+    -- autosave
+    {
+        "okuuva/auto-save.nvim",
+        version = '*', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+        -- lazyload not good
+        -- cmd = "ASToggle", -- optional for lazy loading on command
+        -- event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+        opts = {
+            -- your config goes here
+            -- or just leave it empty :)
+            -- diaplay save message
+        },
+    },
+
 
     -- git
     -- =======================================================
---
+++ lua/plugins-config/auto-save.lua
@@ -0,0 +1,53 @@
+-- auto-save.lua
+-- configurations for auto-save
+
+local plugin_name = "auto-save"
+
+-- =========================================
+-- add some auto cmd
+-- add a augroup
+local group = vim.api.nvim_create_augroup('autosave', {})
+-- display message when auto save a buffer
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'AutoSaveWritePost',
+    group = group,
+    callback = function(opts)
+        if opts.data.saved_buffer ~= nil then
+            local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
+            -- vim.notify('AutoSave: saved ' .. filename .. ' at ' .. vim.fn.strftime('%H:%M:%S'), vim.log.levels.INFO)
+            -- use nvim notify
+            local relative_path = vim.fn.fnamemodify(filename, ':.') -- 相对文爱你路径（项目根）
+            nvim_notify('AutoSave: saved ' .. relative_path .. ' at ' .. vim.fn.strftime('%H:%M:%S'), vim.log.levels.INFO, {
+                title = plugin_name,
+                icon = "💾",
+            })
+        end
+    end,
+})
+-- display message when auto-save enable/disable
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'AutoSaveEnable',
+    group = group,
+    callback = function(opts)
+        -- vim.notify('AutoSave enabled', vim.log.levels.INFO)
+        -- use nvim notify
+        nvim_notify('AutoSave enabled', vim.log.levels.INFO, {
+            title = plugin_name,
+            icon = "💾",
+        })
+    end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'AutoSaveDisable',
+    group = group,
+    callback = function(opts)
+        -- vim.notify('AutoSave disabled', vim.log.levels.INFO)
+        -- use nvim notify
+        nvim_notify('AutoSave disabled', vim.log.levels.INFO, {
+            title = plugin_name,
+            icon = "💾",
+        })
+    end,
+})
+-- =========================================
