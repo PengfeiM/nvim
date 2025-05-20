ef2bd4258eb6c80731877e93b22d9d280828889d
pfma <45191163+PengfeiM@users.noreply.github.com>
2025-05-20 11:49:43 +0800

    feat: protobuf ls
    buf_ls
    brew install buf

---
+++ lua/lsp/bufls.lua
@@ -0,0 +1,23 @@
+-- bufls.lua
+
+
+local nvim_lsp = require('lspconfig')
+local utils = require('lsp.utils')
+
+nvim_lsp.buf_ls.setup({
+    -- keybinding
+    -- on_attach = utils.LspKeybind,
+
+
+    -- cmd, installed by `brew install buf`
+    cmd = { "buf", "beta", "lsp" },
+    -- support file types
+    filetypes = {
+        "proto",
+    },
+    root_dir = nvim_lsp.util.root_pattern(
+        "buf.yaml",
+        ".git"
+    ), -- 根目录
+    capabilities = require('cmp_nvim_lsp').default_capabilities(), -- 自动补全支持
+})
