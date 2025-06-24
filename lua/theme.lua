76fb64c111971e7e37ee507bdee0c85441429ceb
pfma <45191163+PengfeiM@users.noreply.github.com>
2025-06-24 15:39:07 +0800

    feat(theme): add onedarkpro

--- lua/theme.lua
+++ lua/theme.lua
@@ -91,7 +91,7 @@
                 -- Style to be applied to different syntax groups
                 -- Value is any valid attr-list value for `:help nvim_set_hl`
                 comments = { italic = true },
-                keywords = { italic = true },
+                keywords = { italic = true, bold = true },
                 functions = { italic = true },
                 -- variables = { italic = true },
                 -- Background styles. Can be "dark", "transparent" or "normal"
@@ -102,6 +102,106 @@
             lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
         })
     --}
+    -- onedark pro
+    --{
+        require("onedarkpro").setup({
+            colors = {}, -- Override default colors or create your own
+            highlights = {}, -- Override default highlight groups or create your own
+            styles = { -- For example, to apply bold and italic, use "bold,italic"
+                types = "italic", -- Style that is applied to types
+                methods = "italic", -- Style that is applied to methods
+                numbers = "NONE", -- Style that is applied to numbers
+                strings = "NONE", -- Style that is applied to strings
+                comments = "italic", -- Style that is applied to comments
+                keywords = "bold,italic", -- Style that is applied to keywords
+                constants = "bold", -- Style that is applied to constants
+                functions = "bold", -- Style that is applied to functions
+                operators = "NONE", -- Style that is applied to operators
+                variables = "NONE", -- Style that is applied to variables
+                parameters = "italic", -- Style that is applied to parameters
+                conditionals = "italic", -- Style that is applied to conditionals
+                virtual_text = "italic", -- Style that is applied to virtual text
+            },
+            filetypes = { -- Override which filetype highlight groups are loaded
+                c = true,
+                comment = true,
+                go = true,
+                html = true,
+                java = true,
+                javascript = true,
+                json = true,
+                lua = true,
+                markdown = true,
+                php = true,
+                python = true,
+                ruby = true,
+                rust = true,
+                scss = true,
+                toml = true,
+                typescript = true,
+                typescriptreact = true,
+                vue = true,
+                xml = true,
+                yaml = true,
+            },
+            plugins = { -- Override which plugin highlight groups are loaded
+                aerial = true,
+                barbar = true,
+                blink_cmp = true,
+                codecompanion = true,
+                copilot = true,
+                dashboard = true,
+                flash_nvim = true,
+                gitgraph_nvim = true,
+                gitsigns = true,
+                hop = true,
+                indentline = true,
+                leap = true,
+                lsp_saga = true,
+                lsp_semantic_tokens = true,
+                marks = true,
+                mini_diff = true,
+                mini_icons = true,
+                mini_indentscope = true,
+                mini_test = true,
+                neotest = true,
+                neo_tree = true,
+                nvim_cmp = true,
+                nvim_bqf = true,
+                nvim_dap = true,
+                nvim_dap_ui = true,
+                nvim_hlslens = true,
+                nvim_lsp = true,
+                nvim_navic = true,
+                nvim_notify = true,
+                nvim_tree = true,
+                nvim_ts_rainbow = true,
+                op_nvim = true,
+                packer = true,
+                persisted = true,
+                polygot = true,
+                rainbow_delimiters = true,
+                render_markdown = true,
+                startify = true,
+                telescope = true,
+                toggleterm = true,
+                treesitter = true,
+                trouble = true,
+                vim_ultest = true,
+                which_key = true,
+                vim_dadbod_ui = true,
+            },
+
+            options = {
+                cursorline = true, -- Use cursorline highlighting?
+                transparency = true, -- Use a transparent background?
+                terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+                lualine_transparency = true, -- Center bar transparency?
+                highlight_inactive_windows = true, -- When the window is out of focus, change the normal background?
+            }
+        })
+    --}
+--}
 -- }
 -- 主题选择
 -- vim.cmd.colorscheme("cyberdream")
