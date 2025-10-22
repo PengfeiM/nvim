547dfa1649051a5c6cf68e49b5ba6f4f903b25f2
pfma <45191163+PengfeiM@users.noreply.github.com>
2025-10-22 11:15:41 +0800

    feat(telescope): add img block preview

---
+++ lua/plugins-config/README.md
@@ -0,0 +1,9 @@
+# PLUGIN DOCUMENT
+
+## telescope
+
+### image preview in file preview 
+use `catimg` as image viewer, you may install it by
+```bash
+brew install catimg
+```
--- lua/plugins-config/telescope.lua
+++ lua/plugins-config/telescope.lua
@@ -5,6 +5,37 @@ if not status then
 end
 
 telescope.setup({
+	defaults = {
+		preview = {
+			mime_hook = function(filepath, bufnr, opts)
+				local is_image = function(filepath)
+					local image_extensions = { "png", "jpg", "jpeg" } -- Supported image formats
+					local split_path = vim.split(filepath:lower(), ".", { plain = true })
+					local extension = split_path[#split_path]
+					return vim.tbl_contains(image_extensions, extension)
+				end
+				if is_image(filepath) then
+					local term = vim.api.nvim_open_term(bufnr, {})
+					local function send_output(_, data, _)
+						for _, d in ipairs(data) do
+							vim.api.nvim_chan_send(term, d .. "\r\n")
+						end
+					end
+					vim.fn.jobstart({
+						"catimg",
+						filepath, -- Terminal image viewer command
+					}, { on_stdout = send_output, stdout_buffered = true, pty = true })
+				else
+					require("telescope.previewers.utils").set_preview_message(
+						bufnr,
+						opts.winid,
+						"Binary cannot be previewed"
+					)
+				end
+			end,
+		},
+	},
+	extensions_list = { "themes", "terms" },
 	extensions = {
 		fzf = {
 			fuzzy = true,
