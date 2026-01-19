fded1645dc1e2ad9ac403d84277b08745c7b2505
Revan.mac.M <45191163+PengfeiM@users.noreply.github.com>
2026-01-19 11:41:11 +0800

    feat(telescope): aerial

--- lua/plugins-config/telescope.lua
+++ lua/plugins-config/telescope.lua
@@ -67,7 +67,7 @@ else
 					-- paste = "<c-p>",
 					paste = "<cr>",
 					-- paste_behind = "<c-P>",
-                    paste_behind = nil,
+					paste_behind = nil,
 					replay = "<c-q>", -- replay a macro
 					delete = "<c-d>", -- delete an entry
 					edit = "<c-e>", -- edit an entry
@@ -77,7 +77,7 @@ else
 					select = "y",
 					-- paste = "p",
 					--- It is possible to map to more than one key.
-					paste = { 'p', '<cr>' },
+					paste = { "p", "<cr>" },
 					paste_behind = "P",
 					replay = "q",
 					delete = "d",
@@ -89,3 +89,5 @@ else
 	})
 	telescope.load_extension("neoclip")
 end
+-- aerial
+telescope.load_extension("aerial")
