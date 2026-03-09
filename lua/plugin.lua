746e35fcdf39a97112299b28547446e8c8654c0f
Revan.mac.M <45191163+PengfeiM@users.noreply.github.com>
2026-03-09 12:02:44 +0800

    feat(undo): add atone
    a undo tree

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -116,6 +116,11 @@ require("lazy").setup({
             debounce_delay = 0, -- delay after which a pending save is executed
         },
     },
+    -- undo tree
+    {
+        "XXiaoA/atone.nvim",
+        cmd = "Atone",
+    },
 
 
     -- git
