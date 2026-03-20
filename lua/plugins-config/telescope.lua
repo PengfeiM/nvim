2300f180d5713e943110b04bfb3177b3e103da72
Revan.mac.M <45191163+PengfeiM@users.noreply.github.com>
2026-03-20 11:17:43 +0800

    feat(neoclip): use c-p

--- lua/plugins-config/telescope.lua
+++ lua/plugins-config/telescope.lua
@@ -33,8 +33,8 @@ else
 			telescope = {
 				i = {
 					select = "<c-y>",
-					-- paste = "<c-p>",
-					paste = "<cr>",
+					paste = "<c-p>",
+					-- paste = "<cr>",
 					-- paste_behind = "<c-P>",
 					paste_behind = nil,
 					replay = "<c-q>", -- replay a macro
@@ -46,7 +46,7 @@ else
 					select = "y",
 					-- paste = "p",
 					--- It is possible to map to more than one key.
-					paste = { "p", "<cr>" },
+					paste = { "p" },
 					paste_behind = "P",
 					replay = "q",
 					delete = "d",
