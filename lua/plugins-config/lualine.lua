470cc6c1d23e3417bd72da658a0b81485e488310
Revan.wsl.M <45191163+PengfeiM@users.noreply.github.com>
2026-03-22 22:25:50 +0800

    fix(lualine): add showcmd and command

--- lua/plugins-config/lualine.lua
+++ lua/plugins-config/lualine.lua
@@ -3,119 +3,128 @@
 
 local status, lualine = pcall(require, "lualine")
 if not status then
-    vim.notify("没有找到 lualine")
-    return
+	vim.notify("没有找到 lualine")
+	return
 end
 
-
 -- 引入 venv 函数
-local lsp_venv = require('lsp.lsp_venv')
-
+local lsp_venv = require("lsp.lsp_venv")
 
 lualine.setup({
-    options = {
-        icons_enabled = true,
-        theme = 'auto',
-        -- theme = 'dracula',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        always_show_tabline = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 100,
-            tabline = 100,
-            winbar = 100,
-        }
-    },
-    sections = {
-        lualine_a = {
-            'mode',
-            -- {
-            --     -- 显示 spell 检查状态
-            --     function()
-            --         if vim.wo.spell then
-            --             return '󰓆 '..vim.o.spelllang
-            --         else
-            --             return ''
-            --         end
-            --     end,
-            --     color = { fg='#21222c'},
-            -- },
-        },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = {
-            'tabs',
-            'filename',
-            -- 'lsp_progress',
-        },
-        lualine_x = {
-            'encoding',
-            'fileformat',
-            {
-                -- function()
-                --     local msg = "No Active Lsp"
-                --     local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-                --     -- local clients = vim.lsp.get_active_clients()
-                --     local clients = vim.lsp.get_clients()
-                --     if next(clients) == nil then
-                --         return msg
-                --     end
-                --     local clients_name = {}
-                --     for _, client in ipairs(clients) do
-                --         local filetypes = client.config.filetypes
-                --         if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                --             -- return "LSP"
-                --             -- return client.name
-                --             table.insert(clients_name, client.name)
-                --         end
-                --     end
-                --     if #clients_name > 0 then
-                --         return table.concat(clients_name, ",")
-                --     end
-                --     return msg
-                -- end,
-                'lsp_status',
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		-- theme = 'dracula',
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		always_show_tabline = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 100,
+			tabline = 100,
+			winbar = 100,
+		},
+	},
+	sections = {
+		lualine_a = {
+			"mode",
+		},
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {
+			"tabs",
+			"filename",
+		},
+		lualine_x = {
+			-- {
+			-- 	function()
+			-- 		local ok, noice = pcall(require, "noice")
+			-- 		if ok then
+			-- 			return noice.api.statusline.message.get()
+			-- 		end
+			-- 		return ""
+			-- 	end,
+			-- 	cond = function()
+			-- 		local ok, noice = pcall(require, "noice")
+			-- 		return ok and noice.api.statusline.message.has()
+			-- 	end,
+			-- 	color = { fg = "#ff9e64" },
+			-- 	max_length = vim.o.columns / 3,
+			-- },
+			{
+				function()
+					local ok, noice = pcall(require, "noice")
+					if ok then
+						return noice.api.statusline.command.get()
+					end
+					return ""
+				end,
+				cond = function()
+					local ok, noice = pcall(require, "noice")
+					return ok and noice.api.statusline.command.has()
+				end,
+				color = { fg = "#ff9e64" },
+			},
+			{
+				function()
+					local ok, noice = pcall(require, "noice")
+					if ok then
+						return noice.api.statusline.mode.get()
+					end
+					return ""
+				end,
+				cond = function()
+					local ok, noice = pcall(require, "noice")
+					return ok and noice.api.statusline.mode.has()
+				end,
+				color = { fg = "#ff9e64" },
+			},
+			"encoding",
+			"fileformat",
+		},
+		-- lualine_y = {'selectioncount', 'searchcount'},
+		lualine_y = {
+			{
+				"lsp_status",
+				icon = "󱙝",
+			},
+			"filetype",
+			lsp_venv.get_lsp_venv,
+		},
+		-- lualine_z = {'progress', 'location'}
+		lualine_z = {
+			"selectioncount",
+			"searchcount",
+			{
+				function()
+					local current_line = vim.fn.line(".") -- 当前行
+					local total_lines = vim.fn.line("$") -- 总行数
+					-- local current_col = vim.fn.col('.')                  -- 当前列
+					local current_col = vim.fn.virtcol(".") -- 当前列, 视觉列，按照显示统计，而非字符数
+					local percent = math.floor((current_line / total_lines) * 100) -- 百分比
 
-                icon = " ",
-            },
-        },
-        -- lualine_y = {'selectioncount', 'searchcount'},
-        lualine_y = { 'filetype', lsp_venv.get_lsp_venv },
-        -- lualine_z = {'progress', 'location'}
-        lualine_z = {
-            'selectioncount',
-            'searchcount',
-            {
-                function()
-                    local current_line = vim.fn.line('.')                -- 当前行
-                    local total_lines = vim.fn.line('$')                 -- 总行数
-                    -- local current_col = vim.fn.col('.')                  -- 当前列
-                    local current_col = vim.fn.virtcol('.')              -- 当前列, 视觉列，按照显示统计，而非字符数
-                    local percent = math.floor((current_line / total_lines) * 100) -- 百分比
-
-                    -- 格式化显示为：83% :101/121 :7
-                    return string.format("%2d%%%% :%d/%d :%d", percent, current_line, total_lines, current_col)
-                end,
-                -- icon = '📜' -- 可选图标，可以根据需要调整
-            }
-        }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
+					-- 格式化显示为：83% :101/121 :7
+					return string.format("%2d%%%% :%d/%d :%d", percent, current_line, total_lines, current_col)
+				end,
+				-- icon = '📜' -- 可选图标，可以根据需要调整
+			},
+		},
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {},
 })
