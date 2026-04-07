cadc9b808000b8a0b0157b3abf1f10f5a6388829
Revan.mac.M <45191163+PengfeiM@users.noreply.github.com>
2026-04-07 15:46:05 +0800

    feat(goimpl): goimpl has fixed by my pr

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -217,8 +217,8 @@ require("lazy").setup({
             'nvim-telescope/telescope-ui-select.nvim',
             -- optional
             "AckslD/nvim-neoclip.lua",
-            -- 'edolphin-ydf/goimpl.nvim',
-            "PengfeiM/goimpl.nvim"
+            'edolphin-ydf/goimpl.nvim',
+            -- "PengfeiM/goimpl.nvim"
         }
     },
 
