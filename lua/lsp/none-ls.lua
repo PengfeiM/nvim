feab0a31ec22cec14fa180564c3084e580f1a9bf
Revan.mac.M <45191163+PengfeiM@users.noreply.github.com>
2026-04-08 18:02:41 +0800

    fix(none-ls): json auto-format

--- lua/lsp/none-ls.lua
+++ lua/lsp/none-ls.lua
@@ -6,16 +6,18 @@ end
 
 local formatting = null_ls.builtins.formatting
 
-local function set_indentation(filetype)
-    if filetype == "json" then
-        vim.bo.shiftwidth = 2
-        vim.bo.tabstop = 2
-    end
+local indent_2_file_types = { "json", "jsonc" }
 
-    -- if filetype == "yaml" then
-    --     vim.bo.shiftwidth = 4
-    --     vim.bo.tabstop = 4
-    -- end
+local function set_indentation(filetype)
+	if vim.tbl_contains(indent_2_file_types, filetype) then
+		vim.bo.shiftwidth = 2
+		vim.bo.tabstop = 2
+	end
+
+	-- if filetype == "yaml" then
+	--     vim.bo.shiftwidth = 4
+	--     vim.bo.tabstop = 4
+	-- end
 end
 
 null_ls.setup({
@@ -39,7 +41,7 @@ null_ls.setup({
 				"html",
 				-- "json",
 				"yaml",
-                "yml",
+				"yml",
 				"graphql",
 				"astro",
 				"markdown",
@@ -47,20 +49,21 @@ null_ls.setup({
 			extra_filetypes = { "njk" },
 			prefer_local = "node_modules/.bin",
 		}),
-        formatting.prettier.with({
-            filetypes = {
-                "json",
-            },
-            -- make indent = 4 for json file
-            extra_args = {"--tab-width", "2"},
-        }),
-        -- add some go functions, 
-        -- 1. gomodifytags
-        -- 2. go impl a interface
-        null_ls.builtins.code_actions.gomodifytags,
-        null_ls.builtins.code_actions.impl,
-        formatting.goimports,
-        formatting.gofumpt,
+		formatting.prettier.with({
+			filetypes = {
+				"json",
+				"jsonc",
+			},
+			-- make indent = 4 for json file
+			extra_args = { "--tab-width", "2" },
+		}),
+		-- add some go functions,
+		-- 1. gomodifytags
+		-- 2. go impl a interface
+		null_ls.builtins.code_actions.gomodifytags,
+		null_ls.builtins.code_actions.impl,
+		formatting.goimports,
+		formatting.gofumpt,
 	},
 	-- 保存自动格式化
 	-- on_attach = function(client)
@@ -68,8 +71,8 @@ null_ls.setup({
 	-- 	--	vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format{ async = true}")
 	-- 	-- end
 	-- end,
-    on_attach = function(client, bufnr)
-        local filetype = vim.bo.filetype
-        set_indentation(filetype)
-    end,
+	on_attach = function(client, bufnr)
+		local filetype = vim.bo.filetype
+		set_indentation(filetype)
+	end,
 })
